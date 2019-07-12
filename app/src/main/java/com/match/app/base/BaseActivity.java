e1282ae5b85c6f0f3fdcf5cd60fdd90e003a99bc
soong <iamssh@126.com>
2019-07-12 17:58:37 +0800

    status bar

--- app/src/main/java/com/match/app/base/BaseActivity.java
+++ app/src/main/java/com/match/app/base/BaseActivity.java
@@ -37,7 +37,6 @@ public abstract class BaseActivity extends AppCompatActivity implements View.OnC
         mContext = this;
         onInitBinding();
         onInit();
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
     }
 
     /*******
--- app/src/main/java/com/match/app/base/BaseFragmentActivity.java
+++ app/src/main/java/com/match/app/base/BaseFragmentActivity.java
@@ -9,7 +9,5 @@ public class BaseFragmentActivity extends FragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
-//        getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
     }
 }
